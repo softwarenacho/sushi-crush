3bd640dfff32e8e858c79bb35996eb39d7240e0b
Nacho Betancourt <trsgarp@gmail.com>
2024-06-16 15:16:29 -0600

    ts and scss for new board

--- app/_components/Board.tsx
+++ app/_components/Board.tsx
@@ -1,64 +1,315 @@
-'use client';
-import { useCallback, useEffect, useState } from 'react';
-import { BoardProps, SushiOption } from '../_interfaces/Board.interface';
-import { generateBoard } from '../_utils/boardHelper';
+import { useEffect, useState } from 'react';
+import styles from '../_styles//Board.module.scss';
 
-const Board: React.FC<BoardProps> = ({ boardSize, sushiOptions }) => {
-  const [board, setBoard] = useState<SushiOption[][]>([[]]);
+const figures = ['onigiri', 'maki', 'nigiri', 'noodle', 'rice', 'temaki'];
+const boardSize = 10;
 
-  const changeBoard = useCallback(() => {
-    const newBoard = generateBoard(boardSize, sushiOptions);
-    setBoard(newBoard);
-  }, [boardSize, sushiOptions]);
+const getRandomFigure = (): string =>
+  figures[Math.floor(Math.random() * figures.length)];
+
+type Cell = string | null;
+type Board = Cell[][];
+type Match = {
+  row: number;
+  col: number;
+  length: number;
+  horizontal: boolean;
+};
+type SwapInfo = {
+  row1: number;
+  col1: number;
+  row2: number;
+  col2: number;
+} | null;
+type MatchIndicator = Match & { score: number };
+
+const generateBoard = (): Board => {
+  let board: Board = Array(boardSize)
+    .fill(null)
+    .map(() => Array(boardSize).fill(null).map(getRandomFigure));
+
+  while (hasMatches(board)) {
+    board = Array(boardSize)
+      .fill(null)
+      .map(() => Array(boardSize).fill(null).map(getRandomFigure));
+  }
+  return board;
+};
+
+const hasMatches = (board: Board): boolean => {
+  for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+      if (
+        (col < boardSize - 2 &&
+          board[row][col] === board[row][col + 1] &&
+          board[row][col] === board[row][col + 2]) ||
+        (row < boardSize - 2 &&
+          board[row][col] === board[row + 1][col] &&
+          board[row][col] === board[row + 2][col])
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const Board = () => {
+  const [board, setBoard] = useState<Board>([]);
+  const [score, setScore] = useState<number>(0);
+  const [selected, setSelected] = useState<[number, number] | null>(null);
+  const [animating, setAnimating] = useState<boolean>(false);
+  const [matches, setMatches] = useState<Match[]>([]);
+  const [matchIndicators, setMatchIndicators] = useState<MatchIndicator[]>([]);
+  const [swapInfo, setSwapInfo] = useState<SwapInfo>(null);
 
   useEffect(() => {
-    if (!board[0][0]) {
-      changeBoard();
+    setBoard(generateBoard());
+  }, []);
+
+  const findMatches = (board: Board): Match[] => {
+    const foundMatches: Match[] = [];
+    for (let row = 0; row < boardSize; row++) {
+      for (let col = 0; col < boardSize; col++) {
+        let matchLength = 1;
+        while (
+          col + matchLength < boardSize &&
+          board[row][col] === board[row][col + matchLength]
+        ) {
+          matchLength++;
+        }
+        if (matchLength >= 3) {
+          foundMatches.push({
+            row,
+            col,
+            length: matchLength,
+            horizontal: true,
+          });
+        }
+        matchLength = 1;
+        while (
+          row + matchLength < boardSize &&
+          board[row][col] === board[row + matchLength][col]
+        ) {
+          matchLength++;
+        }
+        if (matchLength >= 3) {
+          foundMatches.push({
+            row,
+            col,
+            length: matchLength,
+            horizontal: false,
+          });
+        }
+      }
     }
-  }, [board, changeBoard]);
+    return foundMatches;
+  };
+
+  const processMatches = (matches: Match[], newBoard: Board): number => {
+    let newScore = 0;
+    matches.forEach((match) => {
+      newScore += match.length;
+      for (let i = 0; i < match.length; i++) {
+        if (match.horizontal) {
+          newBoard[match.row][match.col + i] = null;
+        } else {
+          newBoard[match.row + i][match.col] = null;
+        }
+      }
+    });
+    return newScore;
+  };
+
+  const handleMatches = (newBoard: Board, initialScore: number) => {
+    let totalScore = initialScore;
+    let foundMatches = findMatches(newBoard);
+
+    if (foundMatches.length > 0) {
+      setMatches(foundMatches);
+      setMatchIndicators(
+        foundMatches.map((match) => ({ ...match, score: match.length })),
+      );
+      setAnimating(true);
+      setTimeout(() => {
+        totalScore += processMatches(foundMatches, newBoard);
+        fillBoard(newBoard);
+        setMatches([]);
+        setMatchIndicators([]);
+        setAnimating(false);
+        setScore(totalScore);
+        handleMatches(newBoard, totalScore);
+      }, 1000);
+    } else {
+      setBoard(newBoard);
+    }
+  };
+
+  const fillBoard = (newBoard: Board) => {
+    for (let col = 0; col < boardSize; col++) {
+      for (let row = boardSize - 1; row >= 0; row--) {
+        if (newBoard[row][col] === null) {
+          for (let fillRow = row; fillRow >= 0; fillRow--) {
+            if (newBoard[fillRow][col] !== null) {
+              newBoard[row][col] = newBoard[fillRow][col];
+              newBoard[fillRow][col] = null;
+              break;
+            }
+          }
+        }
+        if (newBoard[row][col] === null) {
+          newBoard[row][col] = getRandomFigure();
+        }
+      }
+    }
+  };
+
+  const swap = (
+    row1: number,
+    col1: number,
+    row2: number,
+    col2: number,
+  ): Board => {
+    const newBoard = board.map((row) => row.slice());
+    [newBoard[row1][col1], newBoard[row2][col2]] = [
+      newBoard[row2][col2],
+      newBoard[row1][col1],
+    ];
+    return newBoard;
+  };
+
+  const handleClick = (row: number, col: number) => {
+    if (animating) return;
+    if (selected) {
+      const [selectedRow, selectedCol] = selected;
+      if (
+        (Math.abs(selectedRow - row) === 1 && selectedCol === col) ||
+        (Math.abs(selectedCol - col) === 1 && selectedRow === row)
+      ) {
+        setSwapInfo({
+          row1: selectedRow,
+          col1: selectedCol,
+          row2: row,
+          col2: col,
+        });
+        setAnimating(true);
+        setTimeout(() => {
+          const newBoard = swap(selectedRow, selectedCol, row, col);
+          handleMatches(newBoard, score);
+          setSelected(null);
+          setSwapInfo(null);
+        }, 1000);
+      } else {
+        setSelected([row, col]);
+      }
+    } else {
+      setSelected([row, col]);
+    }
+  };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: `repeat(${boardSize}, 3rem)`,
-          gridTemplateRows: `repeat(${boardSize}, 3rem)`,
-          background: 'white',
-        }}
-      >
-        {board.map((row, rowIndex) =>
-          row.map((sushi, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              style={{
-                width: '3rem',
-                height: '3rem',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                border: '1px solid #1e1e1e',
-                textAlign: 'center',
-                backgroundImage: `url('/sushi/${sushi}.webp')`,
-                backgroundSize: '90%',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-              }}
-            ></div>
-          )),
-        )}
+    <div className={styles.game}>
+      <div className={styles.score}>Score: {score}</div>
+      <div className={styles.board}>
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className={styles.row}>
+            {row.map((cell, colIndex) => (
+              <div
+                key={colIndex}
+                className={`${styles.cell} ${
+                  selected &&
+                  selected[0] === rowIndex &&
+                  selected[1] === colIndex
+                    ? styles.selected
+                    : ''
+                } 
+                  ${
+                    swapInfo &&
+                    swapInfo.row1 === rowIndex &&
+                    swapInfo.col1 === colIndex
+                      ? styles.swap
+                      : ''
+                  } 
+                  ${
+                    swapInfo &&
+                    swapInfo.row2 === rowIndex &&
+                    swapInfo.col2 === colIndex
+                      ? styles.swap
+                      : ''
+                  } 
+                  ${
+                    matches.some(
+                      (match) =>
+                        match.row === rowIndex &&
+                        match.col <= colIndex &&
+                        colIndex < match.col + match.length &&
+                        match.horizontal,
+                    )
+                      ? styles.match
+                      : ''
+                  } 
+                  ${
+                    matches.some(
+                      (match) =>
+                        match.col === colIndex &&
+                        match.row <= rowIndex &&
+                        rowIndex < match.row + match.length &&
+                        !match.horizontal,
+                    )
+                      ? styles.match
+                      : ''
+                  }`}
+                onClick={() => handleClick(rowIndex, colIndex)}
+                style={{
+                  transform: swapInfo
+                    ? swapInfo.row1 === rowIndex && swapInfo.col1 === colIndex
+                      ? `translate(${(swapInfo.col2 - swapInfo.col1) * 40}px, ${
+                          (swapInfo.row2 - swapInfo.row1) * 40
+                        }px)`
+                      : swapInfo.row2 === rowIndex && swapInfo.col2 === colIndex
+                      ? `translate(${(swapInfo.col1 - swapInfo.col2) * 40}px, ${
+                          (swapInfo.row1 - swapInfo.row2) * 40
+                        }px)`
+                      : 'none'
+                    : 'none',
+                  transition: swapInfo ? 'transform 1s' : 'none',
+                  backgroundImage: `url('/sushi/${cell}.webp')`,
+                }}
+              ></div>
+            ))}
+          </div>
+        ))}
+        {matchIndicators.map((indicator, index) => (
+          <div
+            key={index}
+            className={styles.matchIndicator}
+            style={{
+              top: `${indicator.row * 40}px`,
+              left: `${
+                indicator.horizontal
+                  ? (indicator.col + indicator.length / 2) * 40 - 20
+                  : indicator.col * 40
+              }px`,
+              transform: `${
+                indicator.horizontal ? 'translateX(-50%)' : 'translateY(-50%)'
+              }`,
+            }}
+          >
+            +{indicator.score}
+          </div>
+        ))}
       </div>
-      <button
-        style={{
-          marginTop: '2rem',
-          height: '4rem',
-          fontFamily: '"Pacifico", cursive',
-          fontSize: '2rem',
-        }}
-        onClick={() => changeBoard()}
-      >
-        Generate
-      </button>
+      {score > 0 && (
+        <button
+          className={styles.generate}
+          onClick={() => {
+            setBoard(generateBoard());
+            setScore(0);
+          }}
+        >
+          Generate
+        </button>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,34 @@
 'use client';
 
-import FakeBoard from '@/app/_components/FakeBoard';
+import Board from '@/app/_components/Board';
 import Image from 'next/image';
-import styles from './page.module.scss';
+import { Suspense } from 'react';
+import styles from './_styles/page.module.scss';
 
 export default function Home() {
   return (
-    // <Suspense fallback={<div>Loading...</div>}>
-    <main className={styles.main}>
-      <Image src='/sushi/onigiri.webp' alt='Onigiri' width={64} height={64} />
-      <h1 className='pacifico'>Sushi Crush</h1>
-      <section>
-        <Image src='/sushi/maki.webp' alt='Maki' width={48} height={48} />
-        <Image src='/sushi/nigiri.webp' alt='Nigiri' width={48} height={48} />
-        <Image src='/sushi/noodle.webp' alt='Noodle' width={48} height={48} />
-        <Image src='/sushi/rice.webp' alt='Rice' width={48} height={48} />
-        <Image src='/sushi/temaki.webp' alt='Temake' width={48} height={48} />
-      </section>
-      <section>
-        <FakeBoard />
-        {/* <Board
-            boardSize={10}
-            sushiOptions={[
-              'onigiri',
-              'maki',
-              'nigiri',
-              'noodle',
-              'rice',
-              'temaki',
-            ]}
-          /> */}
-      </section>
-      <p>
-        Dive into the delicious world of Sushi Crush, a delightful puzzle game
-        where you match and crush sushi pieces to progress through exciting
-        levels. Combine your favorite sushi types and create mouth-watering
-        combos in this charming and visually stunning game. Perfect for sushi
-        lovers and puzzle enthusiasts alike!
-      </p>
-    </main>
-    // </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className={styles.main}>
+        <Image src='/sushi/onigiri.webp' alt='Onigiri' width={64} height={64} />
+        <h1 className='pacifico'>Sushi Crush</h1>
+        <section>
+          <Image src='/sushi/maki.webp' alt='Maki' width={48} height={48} />
+          <Image src='/sushi/nigiri.webp' alt='Nigiri' width={48} height={48} />
+          <Image src='/sushi/noodle.webp' alt='Noodle' width={48} height={48} />
+          <Image src='/sushi/rice.webp' alt='Rice' width={48} height={48} />
+          <Image src='/sushi/temaki.webp' alt='Temake' width={48} height={48} />
+        </section>
+        <section>
+          <Board />
+        </section>
+        <p>
+          Dive into the delicious world of Sushi Crush, a delightful puzzle game
+          where you match and crush sushi pieces to progress through exciting
+          levels. Combine your favorite sushi types and create mouth-watering
+          combos in this charming and visually stunning game. Perfect for sushi
+          lovers and puzzle enthusiasts alike!
+        </p>
+      </main>
+    </Suspense>
   );
 }
