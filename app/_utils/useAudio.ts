8fec4e4dfb1601455bd780fd7e5baca6ec9570fb
Nacho Betancourt <trsgarp@gmail.com>
2024-06-20 20:39:37 -0600

    fix Audio undefined

--- app/_utils/useAudio.ts
+++ app/_utils/useAudio.ts
@@ -4,10 +4,11 @@ const usePoint = (url: string, volume: number = 0.5) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    const audioElement = new Audio(url);
-    audioElement.volume = volume;
-
-    audioRef.current = audioElement;
+    if (typeof window !== 'undefined') {
+      const audioElement = new Audio(url);
+      audioElement.volume = volume;
+      audioRef.current = audioElement;
+    }
   }, [url, volume]);
 
   const play = () => {
